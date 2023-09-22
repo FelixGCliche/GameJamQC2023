abb9a18caeae9f4a56c2e0da28a9257bfd8e2819
BlackMarcel <antonymenard2014@gmail.com>
2023-09-22 17:51:16 -0400

    add pause menu

---
+++ Gameplay/GameController.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+
+
+public partial class GameController : Node2D
+{
+	private Control pauseMenu;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		pauseMenu = this.GetNode<Control>("pauseMenu");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionPressed("Pause"))
+		{
+			pauseMenu.Visible = true;
+			GetTree().Paused = true;
+		}
+	}
+}
