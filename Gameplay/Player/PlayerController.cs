615bb98d61c605d9b82327342f5d8fdb09335542
Bobbyfast <m.m.desautels@gmail.com>
2023-09-23 14:21:15 -0400

    add IColorable

--- Gameplay/Player/PlayerController.cs
+++ Gameplay/Player/PlayerController.cs
@@ -2,76 +2,115 @@ using Godot;
 
 namespace GameJamQC2023.Player
 {
-    public partial class PlayerController : CharacterBody2D
+    public partial class PlayerController : CharacterBody2D, IColorable
     {
-        [Export]
-        private float acceleration = 250.0f;
-        
-        [Export]
-        private float deceleration = 200.0f;
+		[Export]
+		private float acceleration = 250.0f;
+		
+		[Export]
+		private float deceleration = 200.0f;
 
-        [Export]
-        private float maxGroundSpeed = 1250.0f;
+		[Export]
+		private float maxGroundSpeed = 1250.0f;
 
-        [Export]
-        private float maxAirSpeed = 600.0f;
+		[Export]
+		private float maxAirSpeed = 600.0f;
 
-        [Export]
-        private float jumpSpeed = -1650.0f;
+		[Export]
+		private float jumpSpeed = -1650.0f;
 
-        [Export]
-        private float jumpVelocityFalloff = -1500f;
+		[Export]
+		private float jumpVelocityFalloff = -1500f;
 
-        [Export]
-        private float fallMultiplier = -0.18f;
+		[Export]
+		private float fallMultiplier = -0.18f;
 
-        [Export]
-        private float shortJumpMultiplier = -0.12f;
+		[Export]
+		private float shortJumpMultiplier = -0.12f;
 
-        private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+		private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+		[Export]
+		private ColorData colorData;
+        public ColorData ColorData => colorData;
 
         public override void _PhysicsProcess(double delta)
-        {
-            var movementVelocity = Velocity;
+		{
+			var movementVelocity = Velocity; 
 
-            if (!IsOnFloor())
-                movementVelocity.Y += gravity * (float) delta;
+			if (!IsOnFloor())
+				movementVelocity.Y += gravity * (float) delta;
 
-            movementVelocity = Move(movementVelocity);
-            movementVelocity = Jump(movementVelocity, (float) delta);
+			movementVelocity = Move(movementVelocity);
+			movementVelocity = Jump(movementVelocity, (float) delta);
 
-            Velocity = movementVelocity;
-            MoveAndSlide();
-            GD.Print(Velocity);
+			Velocity = movementVelocity;
+			MoveAndSlide();
+
+
+			var collisionInfo = MoveAndCollide(movementVelocity * (float)delta);
+			HandleColorChange(collisionInfo);
+		}
+
+		private Vector2 Move(Vector2 velocity)
+		{
+			var direction = Input.GetAxis("MoveLeft", "MoveRight");
+
+			if (direction != 0)
+				velocity.X += direction * acceleration;
+			else
+				velocity.X = Mathf.MoveToward(Velocity.X, 0, deceleration);
+
+			velocity.X = IsOnFloor() ? Mathf.Clamp(velocity.X, -maxGroundSpeed, maxGroundSpeed) : Mathf.Clamp(velocity.X, -maxAirSpeed, maxAirSpeed);
+			return velocity;
+		}
+
+		private Vector2 Jump(Vector2 velocity, float delta)
+		{
+			var jumped = Input.IsActionJustPressed("Jump");
+			var jumping = Input.IsActionPressed("Jump");
+
+			if (IsOnFloor() && jumped)
+				velocity.Y = jumpSpeed;
+			
+			if(velocity.Y > jumpVelocityFalloff)
+				velocity += Vector2.Up * gravity * fallMultiplier;
+			else if (velocity.Y < 0 && !jumping)
+				velocity += Vector2.Up * gravity * shortJumpMultiplier;
+			
+			return velocity;
+		}
+
+		private void HandleColorChange(KinematicCollision2D collision)
+		{
+			if (collision == null)
+				return;
+
+            if (collision.GetCollider() is Platform platform)
+            {
+                GD.Print($"Platform {platform}");
+                ReceiveColorData(platform.ColorData);
+            }
         }
 
-        private Vector2 Move(Vector2 velocity)
+        public void SendColorData()
         {
-            var direction = Input.GetAxis("MoveLeft", "MoveRight");
-
-            if (direction != 0)
-                velocity.X += direction * acceleration;
-            else
-                velocity.X = Mathf.MoveToward(Velocity.X, 0, deceleration);
-
-            velocity.X = IsOnFloor() ? Mathf.Clamp(velocity.X, -maxGroundSpeed, maxGroundSpeed) : Mathf.Clamp(velocity.X, -maxAirSpeed, maxAirSpeed);
-            return velocity;
+            throw new System.NotImplementedException();
         }
 
-        private Vector2 Jump(Vector2 velocity, float delta)
+        public void ReceiveColorData(ColorData colorData)
         {
-            var jumped = Input.IsActionJustPressed("Jump");
-            var jumping = Input.IsActionPressed("Jump");
+            throw new System.NotImplementedException();
+        }
 
-            if (IsOnFloor() && jumped)
-                velocity.Y = jumpSpeed;
-            
-            if(velocity.Y > jumpVelocityFalloff)
-                velocity += Vector2.Up * gravity * fallMultiplier;
-            else if (velocity.Y < 0 && !jumping)
-                velocity += Vector2.Up * gravity * shortJumpMultiplier;
-            
-            return velocity;
+        public void BlendColorData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DropColorData()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
