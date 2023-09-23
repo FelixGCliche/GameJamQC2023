c8b5b087300461c59fa1969451c7c8b56b1291f6
unknown <felix.gagne.cliche@gmail.com>
2023-09-23 02:26:20 -0400

    tweek move physics settings

--- Gameplay/Player/PlayerController.cs
+++ Gameplay/Player/PlayerController.cs
@@ -5,16 +5,28 @@ namespace GameJamQC2023.Player
     public partial class PlayerController : CharacterBody2D
     {
         [Export]
-        private float speed = 600.0f;
+        private float acceleration = 250.0f;
+        
+        [Export]
+        private float deceleration = 200.0f;
 
         [Export]
-        private float jumpVelocity = -400.0f;
+        private float maxGroundSpeed = 1250.0f;
 
         [Export]
-        private float fallMultiplier = -2.5f;
+        private float maxAirSpeed = 600.0f;
 
         [Export]
-        private float shortJumpMultiplier = -2.0f;
+        private float jumpSpeed = -1650.0f;
+
+        [Export]
+        private float jumpVelocityFalloff = -1500f;
+
+        [Export]
+        private float fallMultiplier = -0.18f;
+
+        [Export]
+        private float shortJumpMultiplier = -0.12f;
 
         private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
@@ -25,38 +37,40 @@ namespace GameJamQC2023.Player
             if (!IsOnFloor())
                 movementVelocity.Y += gravity * (float) delta;
 
-            var direction = Input.GetAxis("MoveLeft", "MoveRight");
-            movementVelocity = Move(movementVelocity, direction);
-            movementVelocity = Jump(movementVelocity, Input.IsActionPressed("Jump"), (float) delta);
+            movementVelocity = Move(movementVelocity);
+            movementVelocity = Jump(movementVelocity, (float) delta);
 
             Velocity = movementVelocity;
             MoveAndSlide();
+            GD.Print(Velocity);
         }
 
-        private Vector2 Move(Vector2 velocity, float direction)
+        private Vector2 Move(Vector2 velocity)
         {
+            var direction = Input.GetAxis("MoveLeft", "MoveRight");
+
             if (direction != 0)
-                velocity.X = direction * speed;
+                velocity.X += direction * acceleration;
             else
-                velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+                velocity.X = Mathf.MoveToward(Velocity.X, 0, deceleration);
+
+            velocity.X = IsOnFloor() ? Mathf.Clamp(velocity.X, -maxGroundSpeed, maxGroundSpeed) : Mathf.Clamp(velocity.X, -maxAirSpeed, maxAirSpeed);
             return velocity;
         }
 
-        private Vector2 Jump(Vector2 velocity, bool jumped, float delta)
+        private Vector2 Jump(Vector2 velocity, float delta)
         {
+            var jumped = Input.IsActionJustPressed("Jump");
+            var jumping = Input.IsActionPressed("Jump");
+
             if (IsOnFloor() && jumped)
-                velocity.Y = jumpVelocity;
-
-            switch (velocity.Y)
-            {
-                case > 0:
-                    velocity += Vector2.Up * gravity * (fallMultiplier - 1f) * delta;
-                    break;
-                case < 0 when !jumped:
-                    velocity += Vector2.Up * gravity * (shortJumpMultiplier - 1f) * delta;
-                    break;
-            }
-
+                velocity.Y = jumpSpeed;
+            
+            if(velocity.Y > jumpVelocityFalloff)
+                velocity += Vector2.Up * gravity * fallMultiplier;
+            else if (velocity.Y < 0 && !jumping)
+                velocity += Vector2.Up * gravity * shortJumpMultiplier;
+            
             return velocity;
         }
     }
