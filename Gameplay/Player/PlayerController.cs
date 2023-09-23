f7693d9d216fdaf31c00c65b0304f11099dc2a64
unknown <felix.gagne.cliche@gmail.com>
2023-09-23 00:32:35 -0400

    add jump

--- Gameplay/Player/PlayerController.cs
+++ Gameplay/Player/PlayerController.cs
@@ -1,42 +1,63 @@
 using Godot;
 
-public partial class PlayerController : CharacterBody2D
+namespace GameJamQC2023.Player
 {
-	[Export]
-	private float speed = 300.0f;
+    public partial class PlayerController : CharacterBody2D
+    {
+        [Export]
+        private float speed = 600.0f;
 
-	[Export]
-	private float fallMultiplier = 2.5f;
+        [Export]
+        private float jumpVelocity = -400.0f;
 
-	[Export] 
-	private float shortJumpMultiplier = 2f;
-	
-	private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+        [Export]
+        private float fallMultiplier = -2.5f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		var velocity = Velocity;
-		
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-		
-		var direction = Input.GetAxis("MoveLeft", "MoveRight");
-		Move(velocity, direction);
-		MoveAndSlide();
-	}
+        [Export]
+        private float shortJumpMultiplier = -2.0f;
 
-	private void Move(Vector2 velocity, float direction)
-	{
-		
-		if (direction != 0)
-			velocity.X = direction * speed;
-		else
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+        private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-		Velocity = velocity;
-	}
+        public override void _PhysicsProcess(double delta)
+        {
+            var movementVelocity = Velocity;
 
-	private void Jump(Vector2 velocity)
-	{
-	}
+            if (!IsOnFloor())
+                movementVelocity.Y += gravity * (float) delta;
+
+            var direction = Input.GetAxis("MoveLeft", "MoveRight");
+            movementVelocity = Move(movementVelocity, direction);
+            movementVelocity = Jump(movementVelocity, Input.IsActionPressed("Jump"), (float) delta);
+
+            Velocity = movementVelocity;
+            MoveAndSlide();
+        }
+
+        private Vector2 Move(Vector2 velocity, float direction)
+        {
+            if (direction != 0)
+                velocity.X = direction * speed;
+            else
+                velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+            return velocity;
+        }
+
+        private Vector2 Jump(Vector2 velocity, bool jumped, float delta)
+        {
+            if (IsOnFloor() && jumped)
+                velocity.Y = jumpVelocity;
+
+            switch (velocity.Y)
+            {
+                case > 0:
+                    velocity += Vector2.Up * gravity * (fallMultiplier - 1f) * delta;
+                    break;
+                case < 0 when !jumped:
+                    velocity += Vector2.Up * gravity * (shortJumpMultiplier - 1f) * delta;
+                    break;
+            }
+
+            return velocity;
+        }
+    }
 }
