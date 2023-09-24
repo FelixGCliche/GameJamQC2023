97495b4a58e71f4611cb8401fe0c45d60f402e43
BlackMarcel <antonymenard2014@gmail.com>
2023-09-24 00:08:14 -0400

    color coded doors

--- Gameplay/Door/Door.cs
+++ Gameplay/Door/Door.cs
@@ -4,11 +4,23 @@ using GameJamQC2023.Player;
 
 public partial class Door : Area2D
 {
+	Sprite2D lockSprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if((bool)GetMeta("isStartDoor"))
-			GetParent().GetNode<PlayerController>("Player").Position = Position;
+		lockSprite = GetNode<Sprite2D>("LockSprite2D");
+
+        if ((bool)GetMeta("isStartDoor"))
+		{
+            lockSprite.Visible = false;
+            GetParent().GetNode<PlayerController>("Player").Position = Position;
+		}
+		else
+		{
+            lockSprite.Modulate = (Color)GetMeta("colorToUnlock");
+            lockSprite.Visible = true;
+		}
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,10 +30,10 @@ public partial class Door : Area2D
 
 	private void OnCollisionEnter(Node2D body)
 	{
-		
-        if (!(bool)GetMeta("isStartDoor"))
+
+        if (!(bool)GetMeta("isStartDoor") && body.GetNode<Sprite2D>("PlayerSprite").SelfModulate == (Color)GetMeta("colorToUnlock"))
 		{
-            body.PrintTree();
+			body.PrintTree();
 			GetTree().ChangeSceneToFile((string)GetMeta("newScenePath"));
         }
             
