f4a0ef66c6f4eb3098e28f23950aba31500757f6
BlackMarcel <antonymenard2014@gmail.com>
2023-09-24 11:38:53 -0400

    scene transition

--- Gameplay/Door/Door.cs
+++ Gameplay/Door/Door.cs
@@ -4,10 +4,14 @@ using GameJamQC2023.Player;
 
 public partial class Door : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	private SceneTransitionRect transitionRect;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-		if((bool)GetMeta("isStartDoor"))
+        transitionRect = GetParent().GetNode<CanvasLayer>("CanvasLayer").GetNode<ColorRect>("SceneTransitionRect") as SceneTransitionRect;
+
+        if ((bool)GetMeta("isStartDoor"))
 			GetParent().GetNode<PlayerController>("Player").Position = Position;
     }
 
@@ -18,11 +22,9 @@ public partial class Door : Area2D
 
 	private void OnCollisionEnter(Node2D body)
 	{
-		
         if (!(bool)GetMeta("isStartDoor"))
 		{
-            body.PrintTree();
-			GetTree().ChangeSceneToFile((string)GetMeta("newScenePath"));
+            transitionRect.transitionTo((string)GetMeta("newScenePath"));
         }
             
     }
---
+++ UI/Screen/SceneTransitionScreen/SceneTransitionRect.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class SceneTransitionRect : ColorRect
+{
+	AnimationPlayer animPlayer;
+	string scenePath;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+		animPlayer = GetNode<AnimationPlayer>("FadePlayer");
+		animPlayer.PlayBackwards("Fade");
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void transitionTo(string newScenePath)
+	{
+		GD.Print(newScenePath);
+		animPlayer.Play("Fade");
+        scenePath = newScenePath;
+    }
+
+	private void OnFadeEnded(StringName anim_name)
+	{
+        GD.Print(scenePath);
+        GetTree().ChangeSceneToFile(scenePath);
+    }
+}
